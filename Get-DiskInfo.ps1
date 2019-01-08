01b52d03fec65a9b446a8bdc463ef84f4b5f56e2
powershellpr0mpt <robert@powershellpr0mpt.com>
2019-01-08 12:18:18 +0100

    function to properly display disk info

--- Get-DiskInfo.ps1
+++ Get-DiskInfo.ps1
@@ -1,34 +1,47 @@
-function Get-DiskInfo {
+function Get-DiskInfo
+{
     [cmdletbinding()]
-    param()
-    begin {
-        $Disks = Get-Disk
-        $Volumes = Get-Volume
-        $Partitions = Get-Partition
-    }
-    process {
-        foreach ($Volume in $Volumes) {
-            $Partition = $Partitions | Where-Object {$_.AccessPaths -eq $Volume.Path -AND $_.AccessPaths -match '\\\\'}
-            $Disk = $Disks | Where-Object {$_.Path -eq $Partition.DiskPath}
-            [PSCustomObject]@{
-                DriveLetter       = $Volume.DriveLetter
-                Label             = $Volume.FileSystemLabel
-                FileSystem        = $Volume.FileSystem
-                Clustersize       = $Volume.AllocationUnitSize
-                HealthStatus      = $Volume.HealthStatus
-                DedupMode         = $Volume.DedupMode
-                DriveType         = $Volume.DriveType
-                VolumeTotalSizeGB = [math]::round(($Volume.Size / 1GB), 2)
-                VolumeFreeSizeGB  = [math]::round(($Volume.SizeRemaining / 1GB), 2)
-                VolumeUsedSizeGB  = [math]::round((($Volume.Size - $Volume.SizeRemaining) / 1GB), 2)
-                DiskFriendlyName  = $Disk.FriendlyName
-                DiskSerialNumber  = $Disk.SerialNumber
-                DiskTotalSizeGB   = [math]::round(($Disk.Size / 1GB), 2)
-                Disknumber        = $Partition.DiskNumber
-                PartitionNumber   = $Partition.PartitionNumber
-                IsActive          = $Partition.IsActive
-                IsBoot            = $Partition.IsBoot
-                DiskId            = $Disk.Path
+    param(
+        [Parameter(Position = 0, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [string[]]$ComputerName = $env:COMPUTERNAME
+    )
+    process
+    {
+        foreach ($Computer in $ComputerName)
+        {
+            try
+            {
+                $Volumes = Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DriveType = 3" -ComputerName $Computer -ErrorAction Stop
+                foreach ($Volume in $Volumes)
+                {
+                    $Partition = Get-CimAssociatedInstance -InputObject $Volume -ResultClass Win32_DiskPartition
+                    $Disk = Get-CimAssociatedInstance -InputObject $Partition -ResultClassName Win32_DiskDrive
+
+                    [PSCustomObject]@{
+                        ComputerName       = $Computer
+                        DriveLetter        = $Volume.Caption
+                        Label              = $Volume.VolumeName
+                        FileSystem         = $Volume.FileSystem
+                        ClusterSize        = $Partition.BlockSize
+                        VolumeTotalSizeGB  = [math]::round(($Volume.Size / 1GB), 2)
+                        VolumeFreeSizeGB   = [math]::round(($Volume.FreeSpace / 1GB), 2)
+                        VolumeUsedSizeGB   = [math]::round((($Volume.Size - $Volume.FreeSpace) / 1GB), 2)
+                        DiskFriendlyName   = $Disk.Caption
+                        DiskTotalSizeGB    = [math]::round(($Disk.Size / 1GB), 2)
+                        DiskSerialNumber   = $Disk.SerialNumber
+                        DiskFirmware       = $Disk.FirmwareRevision
+                        DiskModel          = $Disk.Model
+                        DiskInterface      = $Disk.InterfaceType
+                        DiskNumber         = $Disk.Index
+                        PartitionNumber    = $Partition.Index
+                        IsPrimaryPartition = $Partition.PrimaryPartition
+                        IsBootPartition    = $Partition.BootPartition
+                    }
+                }
+            }
+            catch
+            {
+                Write-Warning "Unable to get WMI properties for computer '$Computer'"
             }
         }
     }
