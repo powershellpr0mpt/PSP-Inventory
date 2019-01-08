71a006c96caab88b0578c97ea7e50b06e1e5c02d
powershellpr0mpt <robert@powershellpr0mpt.com>
2019-01-08 20:01:57 +0100

    cause dcom to properly try/catch fail if component is unreachable

--- Get-NicInfo.ps1
+++ Get-NicInfo.ps1
@@ -55,49 +55,56 @@ function Get-NicInfo
                 $CimOptions = New-CimSessionOption -Protocol DCOM
                 $CimSession = New-CimSession -ComputerName $Computer -SessionOption $CimOptions
 
-                $Adapters = Get-CimInstance -CimSession $CimSession -ClassName Win32_NetworkAdapter -Filter "Availability =3"  -ErrorAction Stop | Where-Object {$_.AdapterTypeId -match '0|9'}
-                if ($Drivers)
+                try
                 {
-                    $SignedDrivers = Get-CimInstance -CimSession $CimSession -ClassName Win32_PnPSignedDriverCIMDataFile
-                }
-                foreach ($Adapter in $Adapters)
-                {
-                    if ($Adapter.Speed)
-                    {
-                        $LinkSpeed = $Adapter.Speed / 1000000
-                    }
-                    else
-                    {
-                        $LinkSpeed = (Get-CimInstance -CimSession $CimSession -Namespace "root/wmi" -Query "SELECT * FROM MSNdis_LinkSpeed" | Where-Object {$_.InstanceName -eq $Adapter.Name}).NdisLinkSpeed / 1000
-                    }
+                    $Adapters = Get-CimInstance -CimSession $CimSession -ClassName Win32_NetworkAdapter -Filter "Availability =3"  -ErrorAction Stop | Where-Object {$_.AdapterTypeId -match '0|9'}
                     if ($Drivers)
                     {
-                        $DriverInfo = Get-CimInstance -CimSession $CimSession -ClassName Win32_PnPSignedDriver -Filter "Description='$($Adapter.Name)'"
+                        $SignedDrivers = Get-CimInstance -CimSession $CimSession -ClassName Win32_PnPSignedDriverCIMDataFile
                     }
-                    $Config = Get-CimInstance -CimSession $CimSession -ClassName Win32_NetworkAdapterConfiguration -Filter "Index = '$($Adapter.DeviceId)'"
+                    foreach ($Adapter in $Adapters)
+                    {
+                        if ($Adapter.Speed)
+                        {
+                            $LinkSpeed = $Adapter.Speed / 1000000
+                        }
+                        else
+                        {
+                            $LinkSpeed = (Get-CimInstance -CimSession $CimSession -Namespace "root/wmi" -Query "SELECT * FROM MSNdis_LinkSpeed" | Where-Object {$_.InstanceName -eq $Adapter.Name}).NdisLinkSpeed / 1000
+                        }
+                        if ($Drivers)
+                        {
+                            $DriverInfo = Get-CimInstance -CimSession $CimSession -ClassName Win32_PnPSignedDriver -Filter "Description='$($Adapter.Name)'"
+                        }
+                        $Config = Get-CimInstance -CimSession $CimSession -ClassName Win32_NetworkAdapterConfiguration -Filter "Index = '$($Adapter.DeviceId)'"
 
-                    [PSCustomObject]@{
-                        ComputerName      = $Computer
-                        Alias             = $Adapter.NetConnectionID
-                        Index             = $Adapter.DeviceId
-                        PhysicalAdapter   = $Adapter.PhysicalAdapter
-                        IPAddress         = $Config.IPAddress
-                        Status            = (Convert-NetworkStatus -NetworkStatus $Adapter.NetConnectionStatus)
-                        MacAddress        = $Adapter.MacAddress
-                        DHCPEnabled       = $Config.DHCPEnabled
-                        DHCPServer        = $Config.DHCPServer
-                        DNSServers        = $Config.DNSServerSearchOrder
-                        Gateway           = $Config.DefaultIPGateway
-                        Subnet            = $Config.IPSubnet
-                        LinkspeedMB       = ($Adapter.Speed / 1000000)
-                        DriverInf         = if ($Drivers) {$DriverInfo.InfName}else {''}
-                        DriverFileName    = if ($Drivers) {$SignedDrivers.Where{$_.Antecedent.DeviceId -eq $DriverInfo.DeviceId}[0].Dependent.Name}else {''}
-                        DriverVersion     = if ($Drivers) {$DriverInfo.DriverVersion}else {''}
-                        DriverDate        = if ($Drivers) {$DriverInfo.DriverDate}else {''}
-                        DriverDescription = if ($Drivers) {$DriverInfo.Description}else {''}
-                        DriverProvider    = if ($Drivers) {$DriverInfo.DriverProviderName}else {''}
-                        NicManufacturer   = if ($Drivers) {$DriverInfo.Manufacturer}else {''}
+                        [PSCustomObject]@{
+                            ComputerName      = $Computer
+                            Alias             = $Adapter.NetConnectionID
+                            Index             = $Adapter.DeviceId
+                            PhysicalAdapter   = $Adapter.PhysicalAdapter
+                            IPAddress         = $Config.IPAddress
+                            Status            = (Convert-NetworkStatus -NetworkStatus $Adapter.NetConnectionStatus)
+                            MacAddress        = $Adapter.MacAddress
+                            DHCPEnabled       = $Config.DHCPEnabled
+                            DHCPServer        = $Config.DHCPServer
+                            DNSServers        = $Config.DNSServerSearchOrder
+                            Gateway           = $Config.DefaultIPGateway
+                            Subnet            = $Config.IPSubnet
+                            LinkspeedMB       = ($Adapter.Speed / 1000000)
+                            DriverInf         = if ($Drivers) {$DriverInfo.InfName}else {''}
+                            DriverFileName    = if ($Drivers) {$SignedDrivers.Where{$_.Antecedent.DeviceId -eq $DriverInfo.DeviceId}[0].Dependent.Name}else {''}
+                            DriverVersion     = if ($Drivers) {$DriverInfo.DriverVersion}else {''}
+                            DriverDate        = if ($Drivers) {$DriverInfo.DriverDate}else {''}
+                            DriverDescription = if ($Drivers) {$DriverInfo.Description}else {''}
+                            DriverProvider    = if ($Drivers) {$DriverInfo.DriverProviderName}else {''}
+                            NicManufacturer   = if ($Drivers) {$DriverInfo.Manufacturer}else {''}
+                        }
                     }
+                }
+                catch
+                {
+                    Write-Warning "Unable to get WMI properties for computer '$Computer'"
                 }
             }
             catch
