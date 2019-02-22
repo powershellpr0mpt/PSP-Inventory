0d885e16f2c3751582c4ff9d0630697c654d90e2
powershellpr0mpt <robert@powershellpr0mpt.com>
2019-02-22 14:46:46 +0100

    scripts turned to module

---
+++ README.md
@@ -0,0 +1,16 @@
+# PSP-Inventory
+
+# Installing PSP-Inventory
+
+    # Install PSP-Inventory from the Powershell Gallery
+    Find-Module PSP-Inventory | Install-Module
+
+### Change Log
+[Change Log can be viewed here](CHANGELOG.md)
+
+### To Contribute
+[Please see the following article](CONTRIBUTION.md)
+
+### License
+[License can be seen here](LICENSE.md)
+
---
+++ build.ps1
@@ -0,0 +1,49 @@
+[cmdletbinding()]
+param(
+    [ValidateSet('Default', 'Build', 'Analyze', 'Test', 'WinZip', 'DeployDev','DeployProd')]
+    $Task = 'Default'
+)
+
+Write-Host "Importing Modules:" -ForegroundColor Yellow
+$Modules = @('Psake', 'BuildHelpers', 'Pester', 'PSScriptAnalyzer', 'PSDeploy')
+
+
+ForEach ($Module in $Modules) {
+    Write-Host "...$Module" -ForegroundColor Yellow
+    If (!(Get-Module -Name $Module -ListAvailable)) {
+        try {
+            $InstallModuleParams = @{
+                Name  = $Module
+                Scope = 'CurrentUser'
+                Force = $true
+            }
+            if ($Module -eq 'Pester') {
+                $InstallModuleParams.SkipPublisherCheck = $true
+            }
+            $null = Find-Module $Module -ErrorAction Stop
+            $null = Install-Module @InstallModuleParams
+            Write-Host "...Importing" -ForegroundColor Green
+            Import-Module $Module
+            Write-Host "...Complete" -ForegroundColor Green
+        }
+        catch {
+            Write-Host "...Not Found!" -ForegroundColor Red
+            Write-Error -Message $_ -ErrorAction Stop
+        }
+    }
+    Else {
+        Write-Host "...Already Loaded" -ForegroundColor Gray
+    }
+}
+Write-Host "Completed Importing Modules`r`n" -ForegroundColor Green
+
+Push-Location $PSScriptRoot
+Write-Verbose "Cleaning current Build variables"
+Get-ChildItem -Path env:\bh* | Remove-Item
+Write-Verbose "Setting Build Variables"
+Set-BuildEnvironment
+
+Write-Host "Executing PSake Build`r`n" -ForegroundColor Green
+
+Invoke-Psake -buildFile .\psake.ps1 -properties $PSBoundParameters -noLogo -taskList $Task
+exit ( [int]( -not $psake.build_success ) )
