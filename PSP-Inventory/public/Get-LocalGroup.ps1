017d237d64928cab738653c0864482bd7583d4ca
powershellpr0mpt <robert@powershellpr0mpt.com>
2019-02-22 15:10:54 +0100

    fix localgroup - incorrect uservariable

--- PSP-Inventory/public/Get-LocalGroup.ps1
+++ PSP-Inventory/public/Get-LocalGroup.ps1
@@ -15,18 +15,19 @@ Function Get-LocalGroup {
         $Date = Get-Date -f 'dd-MM-yyyy HH:mm:ss'
     }
     process {
-        foreach ($Computer in $Computername) {
-            $GroupInfo = ([ADSI]"WinNT://$Computer").Children | ? {$_.SchemaClassName -eq 'Group'}
+        foreach ($Computer in $ComputerName) {
+            $GroupInfo = ([ADSI]"WinNT://$Computer").Children | Where-Object {$_.SchemaClassName -eq 'Group'}
             foreach ($Group in $GroupInfo) {
-                $Grp = [pscustomobject]@{
+                $Grp = [PSCustomObject]@{
                     ComputerName  = $Computer
                     GroupName     = $Group.Name[0]
                     Members       = ((Get-LocalGroupMember -Group $Group) -join '; ')
                     GroupType     = $GroupType[[int]$Group.GroupType[0]]
-                    SID           = (ConvertTo-SID -BinarySID $User.ObjectSid[0])
+                    SID           = (ConvertTo-SID -BinarySID $Group.ObjectSid[0])
                     InventoryDate = $Date
                 }
                 $Grp.PSTypeNames.Insert(0, 'PSP.Inventory.LocalGroup')
+                $Grp
             }
         }
     }
