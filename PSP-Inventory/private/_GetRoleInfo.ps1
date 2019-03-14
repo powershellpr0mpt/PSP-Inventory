de3df1359257dc5664db47d320f18ced94c598c5
powershellpr0mpt <robert@powershellpr0mpt.com>
2019-03-14 11:28:46 +0100

    Check if serverrole run on workstation. Closes #25

--- PSP-Inventory/private/_GetRoleInfo.ps1
+++ PSP-Inventory/private/_GetRoleInfo.ps1
@@ -4,13 +4,19 @@ function _GetRoleInfo {
         [Microsoft.Management.Infrastructure.CimSession]$Cimsession
     )
     Write-Verbose "[$($CimSession.ComputerName)] - Gathering Server Role information"
-    Get-CimInstance -CimSession $CimSession -ClassName Win32_ServerFeature -Property Id, Name -ErrorAction Stop | ForEach-Object {
-        [PSCustomObject]@{
-            PSTypeName    = 'PSP.Inventory.ServerRole'
-            ComputerName  = $Cimsession.ComputerName
-            RoleId        = $_.Id
-            Name          = $_.Name
-            InventoryDate = (Get-Date)
+    #Check if Workstation - https://docs.microsoft.com/en-us/windows/desktop/CIMWin32Prov/win32-operatingsystem#properties
+    $ProductType = (Get-CimInstance -CimSession $CimSession -ClassName Win32_OperatingSystem -Property ProductType).ProductType
+    if (!($ProductType -eq 1)){
+        Get-CimInstance -CimSession $CimSession -ClassName Win32_ServerFeature -Property Id, Name -ErrorAction Stop | ForEach-Object {
+            [PSCustomObject]@{
+                PSTypeName    = 'PSP.Inventory.ServerRole'
+                ComputerName  = $CimSession.ComputerName
+                RoleId        = $_.Id
+                Name          = $_.Name
+                InventoryDate = (Get-Date)
+            }
         }
+    }else{
+        Write-Warning "[$($CimSession.ComputerName)] - is a workstation, no roles available"
     }
 }
